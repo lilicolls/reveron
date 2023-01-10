810116f65d6d3f266365e9f74b37e38e33bed2ae
Liliana Nazareth Colls Garcia <liliana.colls@autodesk.com>
2023-01-10 14:07:03 -0300

    first commit with strcture

---
+++ src/views/landingPages/Home/Home.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Container from 'common/Container';
+import { DemoPages, Features, Footer, Hero, Highlights } from './components';
+
+const Home = () => {
+  const theme = useTheme();
+  return (
+    <Box>
+      <Box bgcolor={theme.palette.alternate.main} position={'relative'}>
+        <Container position="relative" zIndex={2}>
+          <Hero />
+        </Container>
+      </Box>
+      <Container>
+        <Highlights />
+      </Container>
+      <Container>
+        <Features />
+      </Container>
+      <Box bgcolor={theme.palette.alternate.main}>
+        <Container>
+          <h1> test test </h1>
+          <DemoPages />
+        </Container>
+      </Box>
+      <Container>
+        <Footer />
+      </Container>
+    </Box>
+  );
+};
+
+export default Home;
